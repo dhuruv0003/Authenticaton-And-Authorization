5490a028d64b30d4548769f6b9f21eea431765c0
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-07-12 03:05:12 +0530

    new commit

--- config/database.js
+++ config/database.js
@@ -4,5 +4,16 @@ const mongoose=require('mongoose');
 require('dotenv').config();
 
 exports.dbconnect=()=>{
-    mongoose.connect()
+    mongoose.connect(process.env.MONGODB_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
+    .then(()=>{
+        console.log("Db connected");
+    })
+    .catch(()=>{
+        console.log("Error in connection");
+        console.error(err);
+        process.exit(1);
+    })
 }
