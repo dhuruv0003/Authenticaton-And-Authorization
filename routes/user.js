ed87a873272b5323ac1a0a373b3c5fb5f32432b3
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-07-13 14:39:48 +0530

    new commit

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,37 @@
 const express=require('express')
 const router=express.Router();
 
-const {signup,login}=require('../controller/Auth')
-
+const {signup,login}=require('../controller/Auth');
+const { auth, isStudent, isAdmin } = require('../MiddleWare/MiddleW');
 
 router.post("/signup",signup);
 router.post("/login",login);
+
+//Using middlewaared for testing protetedroutes
+
+router.get('/test',auth,(req,res)=>{
+    res.json({
+        success:true,
+        message:"Protected routes for test"
+    })
+})
+
+// Proteted route for student
+
+router.get('/student',auth,isStudent,(req,res)=>{
+    res.json({
+        success:true,
+        message:"Protected routes for student"
+    })
+})
+
+// Proteted route for student
+
+router.get('/admin',auth,isAdmin,(req,res)=>{
+    res.json({
+        success:true,
+        message:"Protected routes for Admin"
+    })
+})
 
 module.exports=router
